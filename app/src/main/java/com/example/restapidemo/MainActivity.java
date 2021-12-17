48fb88fbe6c6490361958fed7676c5f30f8c46fe
aalshawa97 <abdullahalshawa@gmail.com>
2021-12-17 12:30:36 -0800

    Get city name

---
+++ app/src/main/java/com/example/restapidemo/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.restapidemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn_cityID, btn_cityName, btn_getWeatherByID, getBtn_getWeatherByName;
+    EditText et_dataInput;
+    ListView lv_weatherReport;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Assign values to each control on the layout.
+        btn_cityID = findViewById(R.id.btn_getCityID);
+        btn_cityName = findViewById(R.id.btn_getCityName);
+    }
+}
